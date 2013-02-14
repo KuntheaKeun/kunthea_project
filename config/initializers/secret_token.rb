98a0d6ab1f4cbf2d100e33e61728b6180f8ac31f
kunthea <sokunthea.keun@yahoo.com>
2013-02-14 09:43:47 +0700

    my firs commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KuntheaProject::Application.config.secret_token = 'c3262dcd72755b0eaf9b0cccc340a882a6c56cb5bcc315f503a9bfa93382170f8a355e09093c962b3e6e674cbe88881d142afa37b0796c70a9e2e7b31e913568'
